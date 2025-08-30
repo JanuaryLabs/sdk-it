b10e05d6bd5623cefcfbf8310114d391baf3206b
ezzabuzaid <ezza@kortext.com>
2025-08-31 02:36:26 +0800

    feat(rpc): expose experimental toAgents func instead of toTools

--- packages/typescript/src/lib/agent/ai-sdk.ts
+++ packages/typescript/src/lib/agent/ai-sdk.ts
@@ -1,4 +1,4 @@
-import { camelcase, spinalcase } from 'stringcase';
+import { camelcase } from 'stringcase';
 
 import {
   type IR,
@@ -7,37 +7,79 @@ import {
   forEachOperation,
 } from '@sdk-it/spec';
 
-export function generateAISDKTools(spec: IR) {
-  const groups: Record<string, string[]> = {};
-  forEachOperation(spec, (entry, operation) => {
-    groups[entry.tag] ??= [];
-    groups[entry.tag].push(createTool(entry, operation));
+export function generateAISDKTools(ir: IR) {
+  const groups: Record<
+    string,
+    {
+      tools: string[];
+      instructions: string;
+      displayName: string;
+      name: string;
+    }
+  > = {};
+  forEachOperation(ir, (entry, operation) => {
+    const tagDef = ir.tags.find((tag) => tag.name === entry.tag);
+    if (!tagDef) {
+      console.warn(`No tag details found for tag: ${entry.tag}`);
+      return;
+    }
+
+    groups[entry.tag] ??= {
+      tools: [],
+      instructions: '',
+      displayName: '',
+      name: '',
+    };
+    groups[entry.tag].tools.push(createTool(entry, operation));
+    groups[entry.tag].instructions = tagDef['x-instructions'];
+    groups[entry.tag].name = tagDef.name;
+    groups[entry.tag].displayName = tagDef['x-name'];
   });
   const imports = [
     `import { z } from 'zod';`,
     `import { tool } from 'ai';`,
     `import * as schemas from './inputs/index.ts';`,
   ];
-  const tools = Object.entries(groups).map(([group, tools]) => {
-    return `export const ${spinalcase(group)} = (context: { client: any }) => ({ ${tools.join(', ')} });`;
-  });
+  const agent = Object.entries(groups).map(
+    ([group, { instructions, tools, displayName }]) => {
+      return `export const ${camelcase(group)} = {
+    name: '${displayName}',
+    instructions: \`${instructions}\`,
+    tools: { ${tools.join(', ')} }
+    }`;
+    },
+  );
+  const handoffs = `export const triage = {
+  name: 'Triage Agent',
+  tools:{${Object.entries(groups).map(([, { name }]) => {
+    return createTransferTool(name);
+  })}}}`;
 
-  return [...imports, ...tools].join('\n\n');
+  return [...imports, ...agent, handoffs].join('\n\n');
 }
 
 function createTool(entry: OperationEntry, operation: TunedOperationObject) {
   const schemaName = camelcase(`${operation.operationId} schema`);
   return `'${operation['x-fn-name']}': tool({
       description: \`${operation.description || operation.summary}\`,
-			type: 'function',
       inputSchema: schemas.${schemaName},
-			execute: async (input) => {
+			execute: async (input, options) => {
 				console.log('Executing ${operation.operationId} tool with input:', input);
+        const context = coerceContext(options.experimental_context);
         const response = await context.client.request(
           '${entry.method.toUpperCase()} ${entry.path}' ,
-          input as any,
+          input,
         );
         return JSON.stringify(response);
       },
+    })`;
+}
+
+function createTransferTool(agentName: string) {
+  return `transfer_to_${agentName}: tool({
+      type: 'function',
+      description: 'Transfer the conversation to the ${agentName}.',
+      inputSchema: z.object({}),
+      execute: async () => ({ agent: '${agentName}' }),
     })`;
 }
